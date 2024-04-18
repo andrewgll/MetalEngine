2c2fbc388bdc4cf690d2eb79e71043c249b34d51
andrii.hladkyi.teacher@gmail.com <andrii.hladkyi.teacher@gmail.com>
2024-04-18 08:32:58 +0300

    Initial Commit

---
+++ Engine/EngineApp.swift
@@ -0,0 +1,17 @@
+//
+//  EngineApp.swift
+//  Engine
+//
+//  Created by Andrii on 18.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct EngineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
